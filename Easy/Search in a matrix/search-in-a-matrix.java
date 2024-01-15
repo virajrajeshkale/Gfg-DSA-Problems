4dda5a018b8706ea2d085687dec64d4892c63430
Virajkale_ <95531986+virajrajeshkale@users.noreply.github.com>
2024-01-15 16:11:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Search in a matrix/search-in-a-matrix.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            
+            int mat[][] = new int[n][m];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                  mat[i][j] = sc.nextInt();
+            }
+            
+            int x = sc.nextInt();
+            
+            System.out.println (new Sol().matSearch(mat, n, m, x));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+class Sol
+{
+    public static int matSearch(int mat[][], int N, int M, int x)
+    {
+         int row =0 , col=mat.length-1;
+         
+         while(row <=mat[0].length-1 && col >=0)
+         {
+             if(mat[col][row]==x)
+             {
+                return 1;
+             }
+             else if (mat[col][row]<x)
+             {
+                 row++;
+             }
+             else if (mat[col][row]>x)
+             {
+                 col--;
+             }
+         }
+         return 0;
+    }
+}
