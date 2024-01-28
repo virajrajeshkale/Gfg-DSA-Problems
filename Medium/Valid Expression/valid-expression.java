1789f8577bf91a659d19f6301a2dfdde5fc0e5e4
Virajkale_ <95531986+virajrajeshkale@users.noreply.github.com>
2024-01-28 21:04:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Valid Expression/valid-expression.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while(t-- > 0){
+            String S = sc.nextLine().trim();
+            Solution ob = new Solution();
+            if(ob.valid(S))
+                System.out.println(1);
+            else
+                System.out.println(0);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    boolean valid(String s) 
+    { 
+        Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+    }
+} 
