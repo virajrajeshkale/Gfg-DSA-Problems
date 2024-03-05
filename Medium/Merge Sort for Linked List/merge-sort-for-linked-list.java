2c38f9035d47d4cc1fd9a31643a5c74b4275330e
Virajkale_ <95531986+virajrajeshkale@users.noreply.github.com>
2024-03-05 11:18:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        Node temp1 = head;
+        
+        while(temp1!=null)
+        {
+            list.add(temp1.data);
+            temp1 = temp1.next;
+        }
+        
+        Collections.sort(list);
+        
+        Node dumy = new Node(-1);
+        Node current = dumy; 
+        
+        for(int i=0;i<list.size();i++)
+        { 
+            Node newnode = new Node( list.get(i));
+            current.next =newnode;
+            current = newnode;
+            
+        }
+        return dumy.next;
+    }
+}
+
+
