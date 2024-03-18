7ed7258e8610fb02200e012e17e5d555cd104252
Virajkale_ <95531986+virajrajeshkale@users.noreply.github.com>
2024-03-18 15:56:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check if string is rotated by two places/check-if-string-is-rotated-by-two-places.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t;
+		t = sc.nextInt();
+		String s12 = sc.nextLine();
+		for(int i=0; i<t; i++){
+		    String s1 = sc.nextLine();
+		    String s2 = sc.nextLine();
+		    
+		    Solution obj = new Solution();
+		    
+		    boolean flag = obj.isRotated(s1, s2);
+		    
+		    if(flag == true) System.out.println("1");
+		    else System.out.println("0");
+		    
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    public static boolean isRotated(String str1, String str2)
+    {
+        if(str1.length()==1||str2.length()==1)
+        return false;
+        int n = str1.length();
+        String s1 =str1.substring(2,n)+str1.substring(0,2);
+        String s2 =str1.substring(n-2,n)+str1.substring(0,n-2);
+        return s1.equals(str2)||s2.equals(str2);
+    }
+    
+}
